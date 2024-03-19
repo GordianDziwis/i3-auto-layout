46855e360f1c762e09d0562eadb4cdc57f06b4b1
Gordian Dziwis <dziwis@metameat.xyz>
2024-03-19 14:20:57 +0100

    Adds some improvements
    Signed-off-by: Gordian Dziwis <dziwis@metameat.xyz>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use anyhow::{Error, Result};
-use tokio::{sync::mpsc};
+use jq_rs::run;
+use serde_json::{json, to_string, to_string_pretty, to_value, Value};
+use tokio::sync::mpsc;
+use tokio_i3ipc::event::{Event, Subscribe, WindowChange};
+use tokio_i3ipc::msg::Msg;
+use tokio_i3ipc::reply::{Node, NodeLayout, NodeType, Rect};
+use tokio_i3ipc::I3;
 use tokio_stream::StreamExt;
-use tokio_i3ipc::{
-    event::{Event, Subscribe, WindowChange},
-    msg::Msg,
-    reply::{Node, NodeLayout, Rect},
-    I3,
-};
 
 #[rustfmt::skip]
 fn split_rect(r: Rect) -> &'static str {
@@ -14,25 +14,73 @@ fn split_rect(r: Rect) -> &'static str {
     else { "split v" }
 }
 
-// walk the tree and determine if `window_id` has tabbed parent
-fn has_tabbed_parent(node: &Node, window_id: usize, tabbed: bool) -> bool {
+fn has_splith_workspace_parent(
+    node: &Node,
+    window_id: usize,
+    is_splith_workspace: bool,
+    children: i64,
+) -> bool {
     if node.id == window_id {
-        tabbed
+        is_splith_workspace
     } else {
         node.nodes.iter().any(|child| {
-            has_tabbed_parent(
+            has_splith_workspace_parent(
                 child,
                 window_id,
-                matches!(node.layout, NodeLayout::Tabbed | NodeLayout::Stacked),
+                matches!(node.layout, NodeLayout::SplitH)
+                    && matches!(node.node_type, NodeType::Workspace)
+                    && matches!(node.nodes.len(), children),
+                children,
             )
         })
     }
 }
 
+fn find_parent(parent: &Node, window_id: usize) -> Option<&Node> {
+    if parent.id == window_id {
+        None
+    } else {
+        parent.nodes.iter().find_map(|child| {
+            if child.id == window_id {
+                Some(parent)
+            } else {
+                find_parent(child, window_id)
+            }
+        })
+    }
+}
+
+fn find_sibling(node: &Node, window_id: usize) -> Option<&Node> {
+    if let Some(parent) = find_parent(node, window_id) {
+        parent.nodes.iter().find(|&child| child.id != window_id)
+    } else {
+        None
+    }
+}
+
+async fn filter_tree(tree: &Node) -> Result<String, String> {
+    let json_tree = to_string(tree).unwrap();
+    let res = run(
+        "def filter_nodes: {layout, name, type, orientation, id, nodes: \
+         [.nodes[]? | filter_nodes]}; filter_nodes",
+        &json_tree,
+    );
+
+    match res {
+        Ok(filtered_tree) => {
+            let parsed_tree: Value = serde_json::from_str(&filtered_tree)
+                .map_err(|e| format!("Error parsing filtered tree: {}", e))?;
+            serde_json::to_string_pretty(&parsed_tree)
+                .map_err(|e| format!("Error formatting tree: {}", e))
+        }
+        Err(e) => Err(format!("Error filtering tree: {}", e)),
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    flexi_logger::Logger::try_with_env()?.start()?;
-    let (send, mut recv) = mpsc::channel::<&'static str>(10);
+    flexi_logger::Logger::try_with_str("debug")?.start()?;
+    let (send, mut recv) = mpsc::channel::<String>(10);
 
     let s_handle = tokio::spawn(async move {
         let mut event_listener = {
@@ -43,24 +91,161 @@ async fn main() -> Result<()> {
 
         let i3 = &mut I3::connect().await?;
 
-        while let Some(Ok(Event::Window(window_data))) = event_listener.next().await {
-            if WindowChange::Focus == window_data.change {
-                let is_tabbed = matches!(
-                    window_data.container.layout,
-                    NodeLayout::Tabbed | NodeLayout::Stacked
-                );
+        while let Some(Ok(Event::Window(window_data))) =
+            event_listener.next().await
+        {
+            if matches!(window_data.change, WindowChange::New) {
+                let tree = &i3.get_tree().await?;
 
-                let (name, tabbed_parent) = (
-                    window_data.container.name,
-                    has_tabbed_parent(&i3.get_tree().await?, window_data.container.id, is_tabbed),
-                );
-                log::debug!("name={:?}, tabbed_parent={}", &name, tabbed_parent);
-
-                if !tabbed_parent {
-                    send.send(split_rect(window_data.container.window_rect))
-                        .await?;
-                }
+                let focused_id = window_data.container.id;
+                let parent = find_parent(tree, focused_id);
+                if let Some(parent) = parent {
+                    match parent {
+                        Node {
+                            nodes,
+                            layout: NodeLayout::SplitH,
+                            node_type: NodeType::Workspace,
+                            ..
+                        } if nodes.len() == 3 => {
+                            log::debug!("{}", filter_tree(tree).await.unwrap());
+                            log::debug!("focused_id: {:#?}", focused_id);
+                            // log::debug!(
+                            //     "parent: {}",
+                            //     filter_tree(parent).await.unwrap()
+                            // );
+                            let msg = format!(
+                                "focus left split v layout tabbed focus right \
+                                 move left",
+                            );
+                            send.send(msg).await?;
+                        }
+                        _ => (),
+                    }
+                };
             }
+            if matches!(window_data.change, WindowChange::Move) {
+                let tree = &i3.get_tree().await?;
+
+                let focused_id = window_data.container.id;
+                let parent = find_parent(tree, focused_id);
+                if let Some(parent) = parent {
+                    match parent {
+                        Node {
+                            nodes,
+                            layout: NodeLayout::SplitH,
+                            node_type: NodeType::Workspace,
+                            ..
+                        } if nodes.len() == 3 && nodes[1].id == focused_id => {
+                            log::debug!("{}", filter_tree(tree).await.unwrap());
+                            log::debug!("focused_id: {:#?}", focused_id);
+                            // log::debug!(
+                            //     "parent: {}",
+                            //     filter_tree(parent).await.unwrap()
+                            // );
+                            let msg = format!(
+                                "focus left split v layout tabbed focus right \
+                                 move left",
+                            );
+                            send.send(msg).await?;
+                        }
+                        _ => (),
+                    }
+                };
+            }
+
+            if matches!(window_data.change, WindowChange::Move) {
+                let tree = &i3.get_tree().await?;
+
+                let focused_id = window_data.container.id;
+                let parent = find_parent(tree, focused_id);
+                if let Some(parent) = parent {
+                    match parent {
+                        Node {
+                            nodes,
+                            layout: NodeLayout::SplitH,
+                            node_type: NodeType::Workspace,
+                            ..
+                        } if nodes.len() == 2 => {
+                            let sibling =
+                                find_sibling(tree, focused_id).unwrap();
+                            match sibling {
+                                Node {
+                                    nodes,
+                                    layout: NodeLayout::Tabbed,
+                                    ..
+                                } if nodes.len() == 1 => {
+                                    log::debug!(
+                                        "{}",
+                                        filter_tree(tree).await.unwrap()
+                                    );
+                                    log::debug!(
+                                        "focused_id: {:#?}",
+                                        focused_id
+                                    );
+                                    // log::debug!(
+                                    //     "parent: {}",
+                                    //     filter_tree(parent).await.unwrap()
+                                    // );
+                                    let msg = format!(
+                                        "[con_id={}] move right",
+                                        nodes.first().unwrap().id
+                                    );
+                                    send.send(msg).await?;
+                                }
+                                _ => (),
+                            };
+
+                            // let msg = format!("[con_id={}] focus", focused_id);
+                            // send.send(msg).await?;
+                        }
+                        _ => (),
+                    }
+                };
+            }
+            if window_data.change == WindowChange::Move {
+                log::debug!("MOVED");
+            }
+
+            // if matches!(
+            //     window_data.change,
+            //     WindowChange::New | WindowChange::Move
+            // ) {
+            //     let tree = &i3.get_tree().await?;
+            //     log::debug!("{}", filter_tree(tree).await.unwrap());
+            //     let is_tabbed = matches!(
+            //         window_data.container.layout,
+            //         NodeLayout::Tabbed | NodeLayout::Stacked
+            //     );
+
+            //     if has_splith_workspace_parent(
+            //         tree,
+            //         window_data.container.id,
+            //         false,
+            //         1,
+            //     ) & !is_tabbed
+            //     {
+            //         let focused_id = window_data.container.id;
+            //         let msg = format!(
+            //             "[con_id={}] split v layout tabbed",
+            //             focused_id
+            //         );
+            //         send.send(msg).await?;
+            //         let sibling = find_sibling(tree, focused_id);
+            //         if let Some(sibling) = sibling {
+            //             if !matches!(
+            //                 sibling.layout,
+            //                 NodeLayout::Tabbed | NodeLayout::Stacked
+            //             ) && sibling.nodes.len() == 0
+            //             {
+            //                 let msg = format!(
+            //                     "[con_id={}] split v layout tabbed",
+            //                     sibling.id
+            //                 );
+            //                 send.send(msg).await?;
+            //             }
+            //         }
+            //     }
+            // }
         }
         log::debug!("Sender loop ended");
         Ok::<_, Error>(())
